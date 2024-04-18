a1b57f3efe8559780844c8f010caf8df443a47a5
Mara Neacsu <146334465+MaraNeacsu@users.noreply.github.com>
2024-04-18 10:51:04 +0200

    updated database

--- Infrastructure/Entities/SubscribersEntity.cs
+++ Infrastructure/Entities/SubscribersEntity.cs
@@ -8,8 +8,15 @@ namespace Infrastructure.Entities
 
     {
         [Key]
-    
-     public string Email { get; set; }=null!;
+
+     
+        public string Email { get; set; } = null;
+        public bool DailyNewsletter { get; set; }
+        public bool AdvertisingUpdates { get; set; }
+        public bool WeekinReview { get; set; }
+        public bool EventUpdates { get; set; }
+        public bool StartupsWeekly { get; set; }
+        public bool Podcasts { get; set; }
 
     }
 }
--- WebApi/Controllers/SubscribeController.cs
+++ WebApi/Controllers/SubscribeController.cs
@@ -13,14 +13,14 @@ public class SubscribeController(ApiContext context) : ControllerBase
     private readonly ApiContext _context = context;
 
     [HttpPost]
-    public async Task<ActionResult> Subscribe(string email)
+    public async Task<ActionResult> Subscribe(SubscribersEntity entity)
     {
         if (ModelState.IsValid)
         { 
-        if(await _context.Subscribers.AnyAsync(x => x.Email == email))
+        if(await _context.Subscribers.AnyAsync(x => x.Email == entity.Email))
 
         return Conflict();
-            _context.Add(new SubscribersEntity { Email = email });
+            _context.Add(entity);
             await _context.SaveChangesAsync();
             return Ok();
         }
